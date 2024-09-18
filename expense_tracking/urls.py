524a8c4addfa214d5ee5ace8c1ad81c1384ac3b8
yadhu2309 <yadhu2309@gmail.com>
2024-09-18 18:02:36 +0530

    user

--- expense_tracking/urls.py
+++ expense_tracking/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('expense.urls'))
+    path('/', include('expense.urls')),
+    path('user/', include('user.urls'))
 ]
